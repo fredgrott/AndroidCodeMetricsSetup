3a0a25ae5787e543f933998f73f5521238043512
Fred Grott <fred.grott@gmail.com>
2012-01-25 16:22:11 -0600

    first commit

---
+++ readme.markdown
@@ -0,0 +1,8 @@
+AndroidCodeMetricsSetup
+=======================
+
+Just a simple fast agile way to setup code metrics for an android project. We just use checkstyle, pmd, and classycle
+and define the files for checkstyle and mpd that the IDE wil use. 
+
+Just to see where Eclipse IDE puts stuff as far as checkstyle, classycle, and pmd. Each plugin likes to put reports 
+in a different format or place.
---
+++ src/com/github/shareme/androidcodemetricssetup/AndroidCodeMetricsSetupActivity.java
@@ -0,0 +1,13 @@
+package com.github.shareme.androidcodemetricssetup;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidCodeMetricsSetupActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
